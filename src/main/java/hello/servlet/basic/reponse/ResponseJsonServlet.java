668c361cfd871459d1ca51912e7810ba2569462b
mwchoen <mwcheon@cudo.co.kr>
2022-04-03 23:28:31 +0900

    HTTP 응답 데이터 조회 - API JSON

--- src/main/java/hello/servlet/basic/reponse/ResponseJsonServlet.java
+++ src/main/java/hello/servlet/basic/reponse/ResponseJsonServlet.java
@@ -28,5 +28,6 @@ public class ResponseJsonServlet extends HttpServlet {
         String result = objectMapper.writeValueAsString(helloData);
         response.getWriter().write(result);
 
+
     }
 }
