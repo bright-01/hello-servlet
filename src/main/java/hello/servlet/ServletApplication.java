3836e92457acd35f7bbdef324125613b89321601
mwchoen <mwcheon@cudo.co.kr>
2022-04-21 08:42:19 +0900

    spring mvc

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +14,12 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+
+	// 아래의 설정값을 application.properties 에서 해줌
+//	@Bean
+//	ViewResolver internalResourceViewResolver(){
+//		return new InternalResourceViewResolver("WEB-INF/views", ".jsp");
+//	}
 
 }
