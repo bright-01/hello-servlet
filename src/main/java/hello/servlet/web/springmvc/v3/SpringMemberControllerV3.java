758ff12ca4d229541d6849fcbf090b27b57b084b
mwchoen <mwcheon@cudo.co.kr>
2022-04-21 09:04:24 +0900

    spring mvc v3 실용적인 방법2

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,12 +4,8 @@ import hello.servlet.domain.Member;
 import hello.servlet.domain.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -19,13 +15,13 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    @GetMapping(value = "/new-form")
     public String newForm(){
        return "new-form";
     }
 
 
-    @RequestMapping("/save")
+    @PostMapping(value= "/save")
     public String save(@RequestParam("username") String username, @RequestParam("age") int age, Model model){
 
         Member member = new Member(username, age);
@@ -36,7 +32,7 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping
+    @GetMapping
     public String members(Model model){
         List<Member> members = memberRepository.findAll();
         model.addAttribute("members", members);
